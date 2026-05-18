ab9adfd9b5b890ce317625c5fb23f117e7898779
Matthew Davis <matt@mattdavis.co.uk>
2026-05-18 19:53:02 +0100

    Use Laravel safety mechanisms.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,11 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 final class User extends Authenticatable
 {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Sleep;
+use Illuminate\Validation\Rules\Password;
 
 final class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +32,116 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCommands();
+        $this->configureDates();
+        $this->enforceHttps();
+        $this->configureModels();
+        $this->configureMorphMap();
+        $this->configurePasswordValidation();
+        $this->configureTesting();
+        $this->configureVite();
+    }
+
+    /**
+     * Configure the application's commands.
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    /**
+     * Configure the application's date handling.
+     */
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    /**
+     * Force the application to use HTTPS.
+     */
+    private function enforceHttps(): void
+    {
+        URL::forceHttps();
+    }
+
+    /**
+     * Configure the application's models.
+     */
+    private function configureModels(): void
+    {
+        Model::unguard();
+        Model::preventSilentlyDiscardingAttributes();
+        Model::preventAccessingMissingAttributes();
+
+        // Prevent lazy loading always...
+        Model::preventLazyLoading();
+
+        // ...but in production, log the violation instead of throwing an exception.
+        if (app()->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(function (Model $model, string $attribute): void {
+                logger()->warning(
+                    'Lazy loading violation detected.',
+                    [
+                        'model' => $model::class,
+                        'attribute' => $attribute,
+                    ]
+                );
+            });
+        }
+    }
+
+    /**
+     * Configure the application's morph map.
+     */
+    private function configureMorphMap(): void
+    {
+        Relation::enforceMorphMap([
+            'user' => User::class,
+        ]);
+    }
+
+    /**
+     * Configure the application's password validation rules.
+     */
+    private function configurePasswordValidation(): void
+    {
+        Password::defaults(function () {
+            if (! app()->isProduction() || ! app()->runningUnitTests()) {
+                return null;
+            }
+
+            return Password::min(8)
+                ->max(255)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised();
+        });
+    }
+
+    /**
+     * Configure the application's testing environment.
+     */
+    private function configureTesting(): void
+    {
+        if (! app()->runningUnitTests()) {
+            return;
+        }
+
+        Http::preventStrayRequests();
+        Sleep::fake();
+    }
+
+    /**
+     * Configure the application's Vite usage.
+     */
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
     }
 }
