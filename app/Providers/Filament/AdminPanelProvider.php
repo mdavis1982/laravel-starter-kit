62b43b4afdb1786d25f580cfc5edc88107fbb58e
Matthew Davis <matt@mattdavis.co.uk>
2026-06-18 10:42:37 +0100

    Add global search key binding to Filament panel configuration.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,6 +40,7 @@ final class AdminPanelProvider extends PanelProvider
             ->defaultAvatarProvider(AvatarProvider::class)
             ->databaseNotifications()
             ->sidebarCollapsibleOnDesktop()
+            ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
