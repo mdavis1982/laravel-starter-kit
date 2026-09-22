821f30344298dd2e43df0bd30189c5950109e0c7
Matthew Davis <matt@mattdavis.co.uk>
2026-09-23 00:22:42 +0100

    Flatten to a single app layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,31 @@
-<x-layouts::root {{ $attributes }}>
-    {{ $slot }}
-</x-layouts::root>
+@props([
+    'htmlClasses' => null,
+    'bodyClasses' => null,
+])
+<!doctype html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="{{ $htmlClasses }}">
+    <head>
+        <meta charset="utf-8">
+
+        @head
+        @fonts
+
+        {{-- Stylesheets --}}
+        @vite('resources/css/app.css')
+        @livewireStyles
+        @stack('stylesheets')
+    </head>
+
+    <body class="{{ $bodyClasses }}">
+        {{ $slot }}
+
+        {{-- Javascript --}}
+        @livewireScripts
+        @vite('resources/js/app.js')
+        @stack('javascript')
+
+        {{-- Debug --}}
+        <x-debug.tailwind-breakpoint-tool/>
+    </body>
+</html>
